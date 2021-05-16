a56f66c9a0a5b67196b360280caad8a4cd915bb7
Raphaël Côté <cotlarrc@msn.com>
2021-05-16 03:47:08 -0400

    fix UI

--- resources/views/main/menu.blade.php
+++ resources/views/main/menu.blade.php
@@ -2,6 +2,7 @@
 <html lang="fr-CA" style="height: 100%">
 <head>
     <link rel="stylesheet" href="css/custom.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>UR!</title>
 </head>
