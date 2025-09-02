b0b95ea5ae24abb5a21734ebdd7663f6d0d38d6c
Raphaël Côté <cotlarrc@msn.com>
2025-09-01 22:31:27 -0400

    disable some bots for now

--- resources/views/main/creating_game.blade.php
+++ resources/views/main/creating_game.blade.php
@@ -38,9 +38,9 @@
                     <div class="p-2">
                         <label for="game-share">Choisis ton adversaire robot
                             <select name="bot_behavior" id="nb_jetons" class="form-control">
-                                <option value="neato">Neato</option>
-                                <option value="lut">LUT</option>
-                                <!--<option value="expecto">Expecto</option>-->
+                                {# <option value="neato">Neato</option> #}
+                                {# <option value="lut">LUT</option> #}
+                                {# <option value="expecto">Expecto</option> #}
                                 <option value="alas">Alas</option>
                                 <option value="fire">Fire</option>
                                 <option value="tunehr">Tunehr</option>
